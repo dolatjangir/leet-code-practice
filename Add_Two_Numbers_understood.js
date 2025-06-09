e13ab9742c77e8f45e04001d027403b80733f1d1
dolatjangir <dolatjangir778@gmail.com>
2025-06-09 18:26:14 +0530

    delete the middle Node of a linked list

---
+++ Add_Two_Numbers_understood.js
@@ -0,0 +1,22 @@
+var addTwoNumbers = function(l1, l2) {
+let n1 = l1.val;
+let squre = 1
+while(l1.next){
+   squre = squre*10
+   n1 = n1 * li.next.val/ (squre*10);
+   l1 = l1.next;
+ 
+}
+  console.log(squre)
+return l1.next
+
+};
+
+console.log(addTwoNumbers([1,2,3],[4,5,6]))
+
+//  if(l1){
+//     l1 = l1.next;
+//  } ;
+//  if(l2){
+//     l2 =l2.next;
+//  };
